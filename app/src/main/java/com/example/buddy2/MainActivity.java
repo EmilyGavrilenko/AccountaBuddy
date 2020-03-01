d729fcc5fe2e26e06b50c0895c6c16f3abbebcf6
Meha99 <45151702+Meha99@users.noreply.github.com>
2020-03-01 00:51:18 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/buddy2/MainActivity.java
+++ app/src/main/java/com/example/buddy2/MainActivity.java
@@ -3,42 +3,38 @@ package com.example.buddy2;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.MenuItem;
-=======
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
->>>>>>> 7de412511d596441a131ccfb167d1df840dea1d4
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if (user != null) {
+//            // Name, email address, and user id
+//            String name = user.getDisplayName();
+//            String email = user.getEmail();
+//            String uid = user.getUid();
+//
+//            Toast.makeText(MainActivity.this, email + " " + uid,
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//        else{
+//
+//        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-<<<<<<< HEAD
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(navListener);
-=======
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // Name, email address, and user id
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String uid = user.getUid();
-
-            Toast.makeText(MainActivity.this, email + " " + uid,
-                    Toast.LENGTH_SHORT).show();
-        }
-        else{
-
-        }
->>>>>>> 7de412511d596441a131ccfb167d1df840dea1d4
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -61,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new ProfileFragment();
                             break;
                     }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
                     return true;
                 }
             };
