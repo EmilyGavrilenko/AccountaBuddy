fe5d80236da45bbfc7615b58d7c0def71c2ce5ae
Meha99 <meha9917@gmail.com>
2020-02-29 20:06:10 -0800

    pushing backend stuff in buddy2

--- app/src/main/java/com/example/buddy2/CreateChallenge.java
+++ app/src/main/java/com/example/buddy2/CreateChallenge.java
@@ -1,13 +1,45 @@
 package com.example.buddy2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class CreateChallenge extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateChallenge extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "Create_Challenge";
+    TextView mchallengename;
+    TextView mchallengeDescript;
+    TextView mchallengeCompletionDate;
+    //TextView mchallengeCompletionTime;
+    FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_challenge);
+        mchallengename = findViewById(R.id.name_of_challenge);
+        mchallengeDescript = findViewById(R.id.challenge_description);
+        mchallengeCompletionDate = findViewById(R.id.completion_day);
+        //mchallengeCompletionTime = findViewById(R.id.completion_time);
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.Create_challenge).setOnClickListener(this);
+
+
     }
-}
+    private void createChallenge(String name, String descript, String date, Integer bet ){
+        Log.d(TAG, "createChallenge:" + name);
+        if (!validateForm()) {
+            return;
+        }
+        // [Start create_challenge]
+        mAuth.Challenge(name, descript, date, bet).addO
+    }
+    }
