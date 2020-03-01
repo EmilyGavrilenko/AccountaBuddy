690d3c95f16a563209b7b871f2e97a5e6895d6df
Kira Hewitt <kira@Kiras-MacBook-Air-2.local>
2020-03-01 00:35:36 -0800

    msg

--- app/src/main/java/com/example/buddy2/MainActivity.java
+++ app/src/main/java/com/example/buddy2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.buddy2;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,21 +16,49 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if (user != null) {
+//            // Name, email address, and user id
+//            String name = user.getDisplayName();
+//            String email = user.getEmail();
+//            String uid = user.getUid();
+//
+//            Toast.makeText(MainActivity.this, email + " " + uid,
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//        else{
+//
+//        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // Name, email address, and user id
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String uid = user.getUid();
-
-            Toast.makeText(MainActivity.this, email + " " + uid,
-                    Toast.LENGTH_SHORT).show();
-        }
-        else{
-
-        }
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_new:
+                            selectedFragment = new NewFragment();
+                            break;
+                        case R.id.nav_friends:
+                            selectedFragment = new SocialFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
